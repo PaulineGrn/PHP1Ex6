46d01fa4baf80876323041110a4f772d50ded336
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-03 11:44:36 +0100

    PHP1Ex6

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Déclaration d'un variable number avec un calcul à l'intérieur -->
+<?php
+$number=140+30/2;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP1Ex6</title>
+</head>
+<body>
+  <!-- Déclaration d'echo pour afficher la variable sur l'index. -->
+  <?php
+  echo$number;
+  ?>
+</body>
+</html>
